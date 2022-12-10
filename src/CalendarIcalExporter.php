0ff8609130feb736ecc8ea5f317814f0a4d74c81
Janborg <jan-github@luenborg.eu>
2022-12-10 14:14:07 +0100

    ecs

--- src/CalendarIcalExporter.php
+++ src/CalendarIcalExporter.php
@@ -13,12 +13,9 @@ declare(strict_types=1);
 namespace Janborg\ContaoIcal;
 
 use Contao\CalendarEventsModel;
-
 use Contao\CalendarModel;
 use Contao\Config;
-
 use Contao\File;
-
 use Contao\StringUtil;
 use Contao\System;
 use Kigkonsult\Icalcreator\Util\DateTimeFactory;
@@ -31,14 +28,13 @@ class CalendarIcalExporter
     {
         $this->objCalendar = $calendar;
 
-        $this->shareDir = System::getContainer()->getParameter('contao.web_dir') . '/share/';
+        $this->shareDir = System::getContainer()->getParameter('contao.web_dir').'/share/';
 
-        $this->exportFileName = isset($calendar->ical_alias) ? $calendar->ical_alias . '.ics' : 'calendar' . $calendar->id . '.ics';
+        $this->exportFileName = isset($calendar->ical_alias) ? $calendar->ical_alias.'.ics' : 'calendar'.$calendar->id.'.ics';
     }
 
     /**
      * Update a particular calendar.
-     *
      */
     public function exportCalendar(): void
     {
@@ -48,18 +44,18 @@ class CalendarIcalExporter
         $this->vCal->setXprop(Vcalendar::X_WR_CALDESC, $this->objCalendar->description);
         $this->vCal->setXprop(Vcalendar::X_WR_TIMEZONE, Config::get('timeZone'));
 
-        $startDate = ("" !== $this->objCalendar->ical_export_start) ? $this->objCalendar->ical_export_start : 0;
-        $endDate = ("" !== $this->objCalendar->ical_export_end) ? $this->objCalendar->ical_export_end : time() + 10 * 365 * 24 * 3600; // in config.yaml ???
+        $startDate = '' !== $this->objCalendar->ical_export_start ? $this->objCalendar->ical_export_start : 0;
+        $endDate = '' !== $this->objCalendar->ical_export_end ? $this->objCalendar->ical_export_end : time() + 10 * 365 * 24 * 3600; // in config.yaml ???
 
         $objEvents = CalendarEventsModel::findCurrentByPid($this->objCalendar->id, $startDate, $endDate);
 
         while ($objEvents->next()) {
-
             $vEvent = new Vevent();
 
             if ($objEvents->addTime) {
                 $vEvent->setDtstart(date(DateTimeFactory::$YmdTHis, $objEvents->startTime), [Vcalendar::VALUE => Vcalendar::DATE_TIME]);
-                if (!isset($objEvents->ignoreEndTime) || $objEvents->ignoreEndTime == 0) {
+
+                if (!isset($objEvents->ignoreEndTime) || 0 === $objEvents->ignoreEndTime) {
                     if ($objEvents->startTime < $objEvents->endTime) {
                         $vEvent->setDtend(date(DateTimeFactory::$YmdTHis, $objEvents->endTime), [Vcalendar::VALUE => Vcalendar::DATE_TIME]);
                     } else {
@@ -70,7 +66,8 @@ class CalendarIcalExporter
                 }
             } else {
                 $vEvent->setDtstart(date(DateTimeFactory::$Ymd, $objEvents->startDate), [Vcalendar::VALUE => Vcalendar::DATE]);
-                if (!isset($objEvents->endDate) || $objEvents->endDate == 0) {
+
+                if (!isset($objEvents->endDate) || 0 === $objEvents->endDate) {
                     $vEvent->setDtend(
                         date(DateTimeFactory::$Ymd, $objEvents->startDate + 24 * 60 * 60),
                         [Vcalendar::VALUE => Vcalendar::DATE]
@@ -83,7 +80,8 @@ class CalendarIcalExporter
                 }
             }
 
-            $vEvent->setSummary(html_entity_decode((isset($this->objCalendar->title_prefix) ? $this->objCalendar->title_prefix . " " : "") . $objEvents->title,
+            $vEvent->setSummary(html_entity_decode(
+                (isset($this->objCalendar->title_prefix) ? $this->objCalendar->title_prefix.' ' : '').$objEvents->title,
                 ENT_QUOTES,
                 'UTF-8'
             ));
@@ -94,7 +92,8 @@ class CalendarIcalExporter
                     "\n",
                     System::getContainer()->get('contao.insert_tag.parser')->replaceInline($objEvents->teaser)
                 )), ENT_QUOTES, 'UTF-8'));
-            };
+            }
+
             if (!empty($objEvents->location)) {
                 $vEvent->setLocation(trim(html_entity_decode($objEvents->location, ENT_QUOTES, 'UTF-8')));
             }
@@ -105,11 +104,11 @@ class CalendarIcalExporter
                 $arg = $arrRepeat['value'];
                 $unit = $arrRepeat['unit'];
 
-                if ($arg == 1) {
+                if (1 === $arg) {
                     $unit = substr($unit, 0, -1);
                 }
 
-                $strtotime = '+ ' . $arg . ' ' . $unit;
+                $strtotime = '+ '.$arg.' '.$unit;
                 $newstart = strtotime($strtotime, $objEvents->startTime);
                 $newend = strtotime($strtotime, $objEvents->endTime);
                 $freq = 'YEARLY';
@@ -129,7 +128,7 @@ class CalendarIcalExporter
                         break;
                 }
 
-                $rrule = array('FREQ' => $freq);
+                $rrule = ['FREQ' => $freq];
 
                 if ($objEvents->recurrences > 0) {
                     $rrule['count'] = $objEvents->recurrences;
@@ -147,7 +146,7 @@ class CalendarIcalExporter
 
         $this->iCalContent = $this->vCal->createCalendar();
 
-        $objICalFile = new File(StringUtil::stripRootDir($this->shareDir . $this->exportFileName));
+        $objICalFile = new File(StringUtil::stripRootDir($this->shareDir.$this->exportFileName));
 
         $objICalFile->write($this->iCalContent);
 
