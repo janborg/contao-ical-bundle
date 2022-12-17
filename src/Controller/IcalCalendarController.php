df774e6cd0deb3a84b3748c689e8cfb62cfc6672
Janborg <jan-github@luenborg.eu>
2022-12-17 13:51:34 +0100

    check permission for calendar

--- src/Controller/IcalCalendarController.php
+++ src/Controller/IcalCalendarController.php
@@ -14,19 +14,29 @@ namespace Janborg\ContaoIcal\Controller;
 
 use Contao\CalendarEventsModel;
 use Contao\CalendarModel;
+use Contao\CoreBundle\Security\ContaoCorePermissions;
 use Contao\StringUtil;
+use Contao\System;
 use Symfony\Component\Routing\Annotation\Route;
 use Janborg\ContaoIcal\Response\CalendarResponse;
 use Janborg\ContaoIcal\CalendarIcalExporter;
+use Symfony\Component\HttpFoundation\Response;
 
-class IcalCalendarController 
+class IcalCalendarController
 {
     #[Route('/ical/event/{alias}', name: 'janborg_calendar_ical_event', defaults: ['_scope' => 'frontend', '_token_check' => true])]
-    public function ical_event(string $alias): CalendarResponse
+    public function ical_event(string $alias): CalendarResponse|Response
     {
         $event = CalendarEventsModel::findByAlias($alias);
 
         $calendar = CalendarModel::findById($event->pid);
+
+        $security = System::getContainer()->get('security.helper');
+
+        if ($calendar->protected && !$security->isGranted(ContaoCorePermissions::MEMBER_IN_GROUPS, StringUtil::deserialize($calendar->groups, true))) {
+            $response = new Response('', 403);
+            return $response;
+        }
 
         $calendarIcalExporter = new CalendarIcalExporter($calendar);
 
@@ -42,9 +52,16 @@ class IcalCalendarController
     }
 
     #[Route('/ical/calendar/{id}', name: 'janborg_calendar_ical_calendar', defaults: ['_scope' => 'frontend', '_token_check' => true])]
-    public function ical_calendar(string $id): CalendarResponse
+    public function ical_calendar(string $id): CalendarResponse|Response
     {
         $calendar = CalendarModel::findOneByID($id);
+
+        $security = System::getContainer()->get('security.helper');
+
+        if ($calendar->protected && !$security->isGranted(ContaoCorePermissions::MEMBER_IN_GROUPS, StringUtil::deserialize($calendar->groups, true))) {
+            $response = new Response('', 403);
+            return $response;
+        }
 
         $calendarIcalExporter = new CalendarIcalExporter($calendar);
 
@@ -55,7 +72,7 @@ class IcalCalendarController
         foreach ($events as $event) {
             $calendarIcalExporter->addEventToVcalendar($event, $calendarIcalExporter->vCal);
         }
-        
+
         $iCalContent = $calendarIcalExporter->vCal;
 
         $calendarResponse = new CalendarResponse($iCalContent, StringUtil::standardize($calendar->title));
