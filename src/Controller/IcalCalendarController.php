cf1e6f24afb5a1978a1db30873018f85b2a7d9ad
Janborg <jan-github@luenborg.eu>
2022-12-18 11:10:42 +0100

    throw Contao Exceptions for 404 and 403

--- src/Controller/IcalCalendarController.php
+++ src/Controller/IcalCalendarController.php
@@ -14,27 +14,61 @@ namespace Janborg\ContaoIcal\Controller;
 
 use Contao\CalendarEventsModel;
 use Contao\CalendarModel;
+use Contao\CoreBundle\Exception\AccessDeniedException;
+use Contao\Date;
+use Contao\CoreBundle\Exception\PageNotFoundException;
+use Contao\CoreBundle\Framework\ContaoFramework;
+use Contao\CoreBundle\Security\Authentication\Token\TokenChecker;
 use Contao\CoreBundle\Security\ContaoCorePermissions;
+use Contao\PageModel;
 use Contao\StringUtil;
-use Contao\System;
 use Janborg\ContaoIcal\CalendarIcalExporter;
 use Janborg\ContaoIcal\Response\CalendarResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\SecurityBundle\Security;
 
 class IcalCalendarController
 {
-    #[Route('/ical/event/{alias}', name: 'janborg_calendar_ical_event', defaults: ['_scope' => 'frontend', '_token_check' => true])]
-    public function ical_event(string $alias): CalendarResponse|Response
+    protected $framework;
+    protected $security;
+    protected $tokenChecker;
+
+    public function __construct(ContaoFramework $framework, Security $security, TokenChecker $tokenChecker)
     {
+        $this->framework = $framework;
+        $this->security = $security;
+        $this->tokenChecker = $tokenChecker;
+    }
+    #[Route('/ical/event/{alias}', name: 'janborg_calendar_ical_event', defaults: ['_scope' => 'frontend', '_token_check' => true])]
+    public function ical_event(Request $request, string $alias): CalendarResponse|Response
+    {
+        // Initialize the Contao framework
+        $this->framework->initialize(true);
+
+        // Set the root page for the domain as the pageModel attribute
+        $root = $this->findFirstPublishedRootByHostAndLanguage($request->getHost(), $request->getLocale());
+
+        if (null !== $root) {
+            $root->loadDetails();
+            $request->attributes->set('pageModel', $root);
+            $GLOBALS['objPage'] = $root;
+        }
+
         $event = CalendarEventsModel::findByAlias($alias);
+        
+        // check, if Event exists
+        if (null === $event) {  
+            throw new PageNotFoundException();
+        }
 
         $calendar = CalendarModel::findById($event->pid);
 
-        $security = System::getContainer()->get('security.helper');
+        if ($calendar->protected && !$this->security->isGranted(ContaoCorePermissions::MEMBER_IN_GROUPS, StringUtil::deserialize($calendar->groups, true))) {
+            
+            throw new AccessDeniedException();
 
-        if ($calendar->protected && !$security->isGranted(ContaoCorePermissions::MEMBER_IN_GROUPS, StringUtil::deserialize($calendar->groups, true))) {
-            return new Response('', 403);
         }
 
         $calendarIcalExporter = new CalendarIcalExporter($calendar);
@@ -48,15 +82,34 @@ class IcalCalendarController
         return new CalendarResponse($iCalContent, StringUtil::standardize($event->title));
     }
 
-    #[Route('/ical/calendar/{id}', name: 'janborg_calendar_ical_calendar', defaults: ['_scope' => 'frontend', '_token_check' => true])]
-    public function ical_calendar(string $id): CalendarResponse|Response
+    #[Route('/ical/calendar/{ical_alias}', name: 'janborg_calendar_ical_calendar', defaults: ['_scope' => 'frontend', '_token_check' => true])]
+    public function ical_calendar(Request $request, string $ical_alias): CalendarResponse|Response
     {
-        $calendar = CalendarModel::findOneByID($id);
+        $calendar = CalendarModel::findOneByIcal_alias($ical_alias);
 
-        $security = System::getContainer()->get('security.helper');
+        // Initialize the Contao framework
+        $this->framework->initialize(true);
 
-        if ($calendar->protected && !$security->isGranted(ContaoCorePermissions::MEMBER_IN_GROUPS, StringUtil::deserialize($calendar->groups, true))) {
-            return new Response('', 403);
+        // Set the root page for the domain as the pageModel attribute
+        $root = $this->findFirstPublishedRootByHostAndLanguage($request->getHost(), $request->getLocale());
+
+        if (null !== $root) {
+            $root->loadDetails();
+            $request->attributes->set('pageModel', $root);
+            $GLOBALS['objPage'] = $root;
+        }
+
+        if (null === $calendar || !$calendar->export_ical) {  
+            
+            throw new PageNotFoundException();
+        
+        }
+
+        // check, if calendar is protected
+        if ($calendar->protected && !$this->security->isGranted(ContaoCorePermissions::MEMBER_IN_GROUPS, StringUtil::deserialize($calendar->groups, true))) {
+            
+            throw new AccessDeniedException();
+
         }
 
         $calendarIcalExporter = new CalendarIcalExporter($calendar);
@@ -72,5 +125,20 @@ class IcalCalendarController
         $iCalContent = $calendarIcalExporter->vCal;
 
         return new CalendarResponse($iCalContent, StringUtil::standardize($calendar->title));
+    }
+
+    protected function findFirstPublishedRootByHostAndLanguage(string $host, string $language): ?PageModel
+    {
+        $t = PageModel::getTable();
+        $columns = ["$t.type='root' AND ($t.dns=? OR $t.dns='') AND ($t.language=? OR $t.fallback='1')"];
+        $values = [$host, $language];
+        $options = ['order' => "$t.dns DESC, $t.fallback"];
+
+        if (!$this->tokenChecker->isPreviewMode()) {
+            $time = Date::floorToMinute();
+            $columns[] = "$t.published='1' AND ($t.start='' OR $t.start<='$time') AND ($t.stop='' OR $t.stop>'$time')";
+        }
+
+        return PageModel::findOneBy($columns, $values, $options);
     }
 }
