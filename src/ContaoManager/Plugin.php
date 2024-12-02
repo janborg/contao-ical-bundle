6c38b9fbc2fb4f8aec7862aed020c40bbf682722
Janborg <jan-github@luenborg.eu>
2024-12-02 19:31:13 +0100

    ecs

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -24,7 +24,6 @@ use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
 use Contao\ManagerPlugin\Routing\RoutingPluginInterface;
 use Janborg\ContaoIcal\JanborgContaoIcalBundle;
 use Symfony\Component\Config\Loader\LoaderResolverInterface;
-use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\RouteCollection;
 
