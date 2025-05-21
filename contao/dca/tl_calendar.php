14c06bf5f05448ffda4a88986a9344f38109fd9c
janborg <jan+github@luenborg.eu>
2025-05-21 19:33:06 +0200

    add option share_ical_events

--- contao/dca/tl_calendar.php
+++ contao/dca/tl_calendar.php
@@ -42,7 +42,7 @@ $GLOBALS['TL_DCA']['tl_calendar']['palettes']['__selector__'] = array_merge(
 
 $GLOBALS['TL_DCA']['tl_calendar']['subpalettes'] = array_merge(
     [
-        'export_ical' => 'ical_alias,ical_prefix,ical_description,ical_export_start,ical_export_end,share_ical',
+        'export_ical' => 'ical_alias,ical_prefix,ical_description,ical_export_start,ical_export_end,share_ical,share_ical_events',
     ],
     $GLOBALS['TL_DCA']['tl_calendar']['subpalettes'],
 );
@@ -61,6 +61,14 @@ $GLOBALS['TL_DCA']['tl_calendar']['fields'] = array_merge(
     ]],
     ['share_ical' => [
         'label' => &$GLOBALS['TL_LANG']['tl_calendar']['share_ical'],
+        'exclude' => true,
+        'filter' => true,
+        'inputType' => 'checkbox',
+        'eval' => ['tl_class' => 'clr m12'],
+        'sql' => "char(1) NOT NULL default ''",
+    ]],
+    ['share_ical_events' => [
+        'label' => &$GLOBALS['TL_LANG']['tl_calendar']['share_ical_events'],
         'exclude' => true,
         'filter' => true,
         'inputType' => 'checkbox',
