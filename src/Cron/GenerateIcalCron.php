4e613ddfdb5dfcded363fb1b70c36214f3562191
janborg <jan+github@luenborg.eu>
2025-05-23 07:18:53 +0200

    Generate ical files for Events and Calendars

--- src/Cron/GenerateIcalCron.php
+++ src/Cron/GenerateIcalCron.php
@@ -17,10 +17,16 @@ declare(strict_types=1);
 namespace Janborg\ContaoIcal\Cron;
 
 use Contao\CalendarModel;
-use Contao\CoreBundle\DependencyInjection\Attribute\AsCronJob;
-use Contao\CoreBundle\Framework\ContaoFramework;
+use Contao\CalendarEventsModel;
 use Janborg\ContaoIcal\CalendarIcalExporter;
+use Contao\CoreBundle\Framework\ContaoFramework;
+use Contao\CoreBundle\DependencyInjection\Attribute\AsCronJob;
 use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
+
+/**
+ * Generates iCal files for calendars and their events on a hourly basis.
+ *
+ */
 
 #[AsCronJob('hourly')]
 class GenerateIcalCron
@@ -34,12 +40,37 @@ class GenerateIcalCron
 
     public function __invoke(): void
     {
-        $calendars = CalendarModel::findBy(['export_ical=?'], [1]);
+        // find all Calendars to be exported
+        $calendars = CalendarModel::findAll(
+            ['export_ical=?'],
+            [true]
+        );
+        
+        if (null === $calendars) {
+            return;
+        }
 
         foreach ($calendars as $calendar) {
-            $calendarExporter = new CalendarIcalExporter($calendar, $this->eventDispatcher);
 
-            $calendarExporter->exportCalendar();
+            // Continue if neither Calender, nor Events are set to be share as file in public/share.
+            if (!$calendar->share_ical && !$calendar->share_ical_events) {
+                continue;
+            }
+
+            // Export ics for the Calendar in public/share.
+            if ($calendar->share_ical) {
+                $calendarExporter = new CalendarIcalExporter($calendar, $this->eventDispatcher);
+                $calendarExporter->exportCalendar();
+            }
+
+            // Export ics for each Event in public/share.
+            if ($calendar->share_ical_events) {
+                $events = CalendarEventsModel::findBy('pid', $calendar->id);
+                foreach ($events as $event) {
+                    $eventExporter = new CalendarIcalExporter($calendar, $this->eventDispatcher);
+                    $eventExporter->exportCalendarEvent($event);
+                }
+            }
         }
     }
 }
