b59a0c8f1bc83ed31c3bd0519c6f22fd69c1cd61
janborg <jan+github@luenborg.eu>
2026-09-03 06:10:06 +0200

    fix: update ecs.php to fluent config API and resolve phpstan errors
    The deprecated closure-based ECS config format triggered a warning
    under contao/easy-coding-standard ^7.1; switch to the fluent
    ECSConfig::configure() API with SetList::CONTAO. Also drop the
    now-always-true !$calendar->protected checks flagged by phpstan,
    since the method already returns early when protected is true.

--- ecs.php
+++ ecs.php
@@ -2,14 +2,12 @@
 
 declare(strict_types=1);
 
+use Contao\EasyCodingStandard\Set\SetList;
 use PhpCsFixer\Fixer\Comment\HeaderCommentFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 
-return static function (ECSConfig $ecsConfig): void {
-    $ecsConfig->sets([__DIR__.'/vendor/contao/easy-coding-standard/config/contao.php']);
-
-    $ecsConfig->ruleWithConfiguration(HeaderCommentFixer::class, [
-        'header' => "This file is part of contao-ical-bundle.\n\n(c) Jan Lünborg 2022 <jan-github@luenborg.eu>\n\n@license MIT\nFor the full copyright and license information,\nplease view the LICENSE file that was distributed with this source code.\n\n@link https://github.com/janborg/contao-ical-bundle",    ]);
-
-    // Adjust the configuration according to your needs.
-};
+return ECSConfig::configure()
+    ->withSets([SetList::CONTAO])
+    ->withConfiguredRule(HeaderCommentFixer::class, [
+        'header' => "This file is part of contao-ical-bundle.\n\n(c) Jan Lünborg 2022 <jan-github@luenborg.eu>\n\n@license MIT\nFor the full copyright and license information,\nplease view the LICENSE file that was distributed with this source code.\n\n@link https://github.com/janborg/contao-ical-bundle",
+    ]);
